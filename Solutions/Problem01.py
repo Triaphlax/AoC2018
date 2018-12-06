f43a6167bb413c3649550a7123b490088671c0b1
Rolf Plagmeijer <sausage948@gmail.com>
2018-12-06 13:13:51 +0100

    Added comments to problems 1 and 2

--- Solutions/Problem01.py
+++ Solutions/Problem01.py
@@ -5,18 +5,26 @@ answer1 = 0
 answer2 = 0
 contents = getContents(1, True)
 
+# Parse the frequencies and put them in a list
 frequencies = list(map(int, contents.split("\n")))
 
 # ------Part 1------ #
+
+#  Sum the entire list of frequencies
 answer1 = sum(frequencies)
 
 # ------Part 2------ #
+
+# Keep track of frequencies we've seen
 seenFrequencies = []
 currentFrequency = 0
 repeatFound = False
+
+# Update the frequency list every step of the way
 while not repeatFound:
     for f in frequencies:
         currentFrequency += f
+        # If we find a frequency we've already encoutered, stop and note the answer
         if currentFrequency in seenFrequencies:
             answer2 = currentFrequency
             repeatFound = True
--- Solutions/Problem02.py
+++ Solutions/Problem02.py
@@ -10,10 +10,15 @@ boxIDs = contents.split("\n")
 # ------Part 1------ #
 letter2 = 0
 letter3 = 0
+
+# Go through the entire list of boxes
 for box in boxIDs:
+    # For a box ID, note how often each char appears
     letterFreqs = {}
     for char in box:
         letterFreqs[char] = letterFreqs.get(char, 0) + 1
+
+    # Count whether or not this word has chars occurring twice or thrice
     hasLetter2 = False
     hasLetter3 = False
     for letter, freq in letterFreqs.items():
@@ -23,6 +28,8 @@ for box in boxIDs:
             hasLetter2 = True
         elif freq == 3 and not hasLetter3:
             hasLetter3 = True
+
+    # Increase our counters if necessary
     if hasLetter2:
         letter2 += 1
     if hasLetter3:
@@ -33,30 +40,42 @@ answer1 = letter2 * letter3
 
 # ------Part 2------ #
 def findMatchingID(boxIndex):
+    """
+    Finds out whether or not a boxID differs by one character from another boxID that comes after in in boxIDs
+    @param boxIndex: Index of the boxID to check correspoding to boxIDs
+    @return: The boxID with the differing chracter removed, or "" if no such match found
+    """
+
+    # Loop through all boxIDs
     currentBoxID = boxIDs[boxIndex]
     for _, otherBoxID in enumerate(boxIDs[boxIndex+1:], start=boxIndex+1):
+
+        # Loop through the string of our boxID
         possibleIndex = -1
         strikes = 0
         solutionPossible = False
         for j, ch in enumerate(currentBoxID):
+            # If a char matches between the two strings
             if currentBoxID[j] != otherBoxID[j]:
                 strikes += 1
-                if strikes == 1:
+                if strikes == 1: # One strike means possible solution
                     solutionPossible = True
                     possibleIndex = j
-                elif strikes == 2:
+                elif strikes == 2: # Two strikes means a solution is no longer possible
                     solutionPossible = False
                     break
         if solutionPossible:
             return currentBoxID[:possibleIndex] + currentBoxID[(possibleIndex+1):]
     return ""
 
-
-answer2 = ""
+# Check for a matching boxID for all boxIDs in our list
+result = ""
 for i in range(0, len(boxIDs)):
-    answer2 = findMatchingID(i)
-    if answer2 != "":
+    result = findMatchingID(i)
+    if result != "":
         break
+
+answer2 = result
 
 # ------Output------ #
 print("Answer 1: " + str(answer1))
